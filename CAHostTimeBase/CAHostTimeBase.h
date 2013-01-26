c978ce80791e967892fa543309892136a0d17694
Павел Литвиненко <gerasim13@gmail.com>
2013-01-26 04:05:36 +0400

    Initial commit.

---
+++ CAHostTimeBase/CAHostTimeBase.h
@@ -0,0 +1,34 @@
+//
+//  CAHostTimeBase.h
+//  CAHostTimeBase
+//
+//  Created by Павел Литвиненко on 26.01.13.
+//  Copyright (c) 2013 Casual Underground. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <mach/mach_time.h>
+
+@interface CAHostTimeBase : NSObject
+
++ (UInt64)convertToNanos:(UInt64)hostTime;
++ (UInt64)convertFromNanos:(UInt64)nanos;
++ (UInt64)getTheCurrentTime;
+#if TARGET_OS_MAC
++ (UInt64)getCurrentTime;
+#endif
++ (UInt64)getCurrentTimeInNanos;
++ (Float64)getFrequency;
++ (Float64)getInverseFrequency;
++ (UInt32)getMinimumDelta;
++ (UInt64)absoluteHostDeltaToNanosInStartTime:(UInt64)startTime inEndTime:(UInt64)endTime;
++ (SInt64)hostDeltaToNanosInStartTime:(UInt64)startTime inEndTime:(UInt64)endTime;
+
++ (SInt64)msecsToNanos:(Float32)msecs;
++ (SInt64)secondsToNanos:(Float32)seconds;
++ (SInt32)nanosToMsecs:(SInt64)nanos;
++ (Float32)nanosToSeconds:(SInt64)nanos;
++ (UInt64)tickTimeFromBpm:(Float32)bpm;
++ (Float32)bpmFromTickTime:(UInt64)tick;
+
+@end
